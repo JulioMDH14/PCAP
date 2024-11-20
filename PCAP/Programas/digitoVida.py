44c0f8bda93cd4e95d94baa80c7c70fbd6b8c5a0
Julio Muñoz <alu-julmunhoc@foc.es>
2024-11-20 17:44:20 +0100

    Cambios en digitoVida.py

--- PCAP/Programas/digitoVida.py
+++ PCAP/Programas/digitoVida.py
@@ -10,7 +10,11 @@ digito = 0
 suma = 0
 
 for c in fecha:
-    digito = int(c)
-    suma = suma + digito
+    suma += int(c)
+if len(str(suma)) > 1:
+    for c in str(suma):
+        digito += int(c)
+else:
+    digito = suma
         
-print("Tu dígito de vida es ", suma)
+print("Tu dígito de vida es ", digito)
